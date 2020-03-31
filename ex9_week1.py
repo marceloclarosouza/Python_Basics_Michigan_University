07b465fad17b15d939cf6a29e2c00e3c9826bc61
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-31 09:23:43 -0300

    final

---
+++ ex9_week1.py
@@ -0,0 +1,6 @@
+#Write a program that will compute the area of a rectangle. Prompt the user to enter the width and height of the rectangle and store the values in variables called width and height. Print a nice message with the answer..
+
+width = float(input("width"))
+height = float(input("length"))
+area = width * height
+print(area)
