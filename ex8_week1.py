7d728c5812b595f4821c05b06c4dc64932ca30bd
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-31 09:18:22 -0300

    final

---
+++ ex8_week1.py
@@ -0,0 +1,5 @@
+# Write a program that will compute the area of a circle. Prompt the user to enter the radius and save it to avariable called radius. Print a nice message back to the user with the answer
+
+radius = float(input("radius"))
+area_circle = 3.1415*(radius**2)
+print(area_circle)
