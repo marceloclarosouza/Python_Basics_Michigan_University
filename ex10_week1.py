613d561b4039977fca2f1ace4477d9e7a826b38d
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-03-31 10:04:52 -0300

    final

---
+++ ex10_week1.py
@@ -0,0 +1,6 @@
+#Write a program that will compute MPG for a car. Prompt the user to enter the number of miles driven and the number of gallons used. Print a nice message with the answer.
+
+miles = float(input("miles"))
+gallons = float(input("gallons"))
+MPG = miles/gallons
+print(MPG)
