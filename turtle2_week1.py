8ead2148bb4eb58a4e47c50f806000983e5ec945
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-01 14:51:17 -0300

    final

---
+++ turtle2_week1.py
@@ -0,0 +1,20 @@
+import turtle
+i = 0
+
+for i in range(10):
+    wn = turtle.Screen()
+    wn.bgcolor("lightgreen")
+
+    tess = turtle.Turtle()
+    tess.color("blue")#make tess blue
+    tess.pensize(3)#set the width of her pen
+
+    tess.forward(50)
+    tess.left(120)
+    tess.forward(50)
+    tess.left(120)
+    tess.forward(50)
+
+
+wn.exitonclick() #wait for a user click on the canvas
+
