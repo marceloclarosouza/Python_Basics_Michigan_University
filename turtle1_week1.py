51a1300fd5ed0c7988e324c5e3ada64363890432
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-01 14:29:14 -0300

    final

---
+++ turtle1_week1.py
@@ -0,0 +1,12 @@
+import turtle
+wn = turtle.Screen() #creates a graphics window
+alex = turtle.Turtle() #create a turtle named Alex
+alex.forward(150) # tell Alex to move forward by 150 units
+alex.left(90) # turn by 90 degrees
+alex.forward (75)# complete the second side of a retangle
+alex.left(90)
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+alex.salary = 50000
+print(alex.salary)
