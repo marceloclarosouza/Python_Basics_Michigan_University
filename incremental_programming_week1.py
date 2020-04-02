7d108cd9ed1a0c8f2f5a93b1656594c227fbdaa7
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-02 08:49:17 -0300

    final

---
+++ incremental_programming_week1.py
@@ -0,0 +1,18 @@
+import turtle
+import math
+wn = turtle.Screen()
+bob = turtle.Turtle()
+bob.right(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+dist = math.sqrt(50*50/2)
+bob.right(135)
+bob.forward(dist)
+bob.right(90)
+bob.forward(dist)
+wn.exitonclick()
