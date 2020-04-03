aa74c778ec4dbe3c2e8cec1ecbf7ba6324ac4140
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-03 12:47:36 -0300

    final

---
+++ week2/ex7_week2.py
@@ -0,0 +1,5 @@
+#Create a variable called wrds and assign to it a list whose elements are the words in the string sent.
+
+sent = "The bicentennial for our university was in 2017"
+wrds = sent.split()
+print(wrds)
