622425d384ed4e386c14cd9d388e730e28c7d48e
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-03 16:06:11 -0300

    final

---
+++ week2/ex8_week2.py
@@ -0,0 +1,3 @@
+#Write code to create a list of integers from 0 through 52 and assign that list to the variable numbers. You should use a special Python function – do not type out the whole list yourself. HINT: You can do this in one line of code!
+numbers = range(53)
+print(numbers)
