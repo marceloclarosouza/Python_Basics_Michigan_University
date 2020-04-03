e9e0641f71759a331f0ce62b8e684f1d08fd36b5
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-03 12:45:15 -0300

    final

---
+++ week2/ex6_week2.py
@@ -0,0 +1,5 @@
+#Assign the number of elements in lst to the variable num_lst.
+
+lst = ["hi", "goodbye", "python", "106", "506", 91, ['all', 'Paul', 'Jackie', "UMSI", 1, "Stephen", 4.5], 109, "chair", "pizza", "wolverine", 2017, 3.92, 1817, "account", "readings", "papers", 12, "facebook", "twitter", 193.2, "snapchat", "leaders and the best", "social", "1986", 9, 29, "holiday", ["women", "olympics", "gold", "rio", 21, "2016", "men"], "26trombones"]
+num_lst = len(lst)
+print(num_lst)
