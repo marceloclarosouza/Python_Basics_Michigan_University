36bf760c84f88213e29e8a0bf8be5b15c8499ccb
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-03 11:21:51 -0300

    final

---
+++ week2/ex2_week2.py
@@ -0,0 +1,8 @@
+#Write code to determine how many t's are in the following sentences
+
+phrases = ["My, what a lovely day today is!",
+"Have you mastered cooking yet? A tasty trest could be in your future.",
+"Have you ever seen the leaves change color?"]
+
+for sentence in phrases:
+    print(sentence.count("t"))
