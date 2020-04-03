6265c04087c68002d2b206c034eb8825e2ce666f
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-03 16:17:13 -0300

    final

---
+++ week2/ex9_week2.py
@@ -0,0 +1,8 @@
+#Count the number of characters in string str1. Do not use len(). Save the number in variable numbs.
+
+str1 = "I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living."
+numbs = 0
+for i in str1:
+    numbs += 1
+
+print(numbs)
