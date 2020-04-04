0ed4f51dcdb276a19cf93e5d2df7f839823ec0e7
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-04 12:12:57 -0300

    final

---
+++ week2/ex20_week2.py
@@ -0,0 +1,8 @@
+#Create an empty string and assign it to the variable lett. Then using range, write code such that when your code is run, lett has 7 b’s ("bbbbbbb").
+
+lett = []
+
+for i in range(8):
+    lett = ("b"*i)
+
+print(lett)
