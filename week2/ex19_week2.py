c09e4963e725a3200cd0e8a37b85f75d9b3ad79a
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-04 12:02:25 -0300

    final

---
+++ week2/ex19_week2.py
@@ -0,0 +1,12 @@
+#Write code to create a list of word lengths for the words in original_str using the accumulation pattern and assign the answer to a variable num_words_list. (You should use the len function).
+
+original_str = "The quick brown rhino jumped over the extremely lazy fox"
+
+word = original_str.split(" ")
+print(word)
+
+num_words_list = []
+for i in word:
+    num_words_list.append(len(i))
+
+print(num_words_list)
