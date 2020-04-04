f03cf21ab1d4c5bd196f11b87314d44f88d97c0c
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-04 10:56:19 -0300

    final

---
+++ week2/ex15_week2.py
@@ -0,0 +1,10 @@
+#Write code to count the number of characters in original_str using the accumulation pattern and assign the answer to a variable num_chars. Do NOT use the len function to solve the problem (if you use it while you are working on this problem, comment it out afterward!)
+
+original_str = "The quick brown rhino jumped over the extremely lazy fox."
+
+count = 0
+for i in original_str:
+    count += 1
+
+num_chars = count
+print(num_chars)
