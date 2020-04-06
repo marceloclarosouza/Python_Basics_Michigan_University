bf5083ce0b8f505fc9339586c15679f51c6211ad
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-06 09:03:43 -0300

    final

--- week3/ex1_week3.py
+++ week3/ex1_week3.py
@@ -3,7 +3,7 @@
 courses = ["ENGR 101", "SI 110", "ENG 125", "SI 106", "CHEM 130"]
 
 if "SI 106" in courses:
-    output = "You can apply to SI"
+    output = "You can apply to SI!"
 else:
     output = "Take SI 106"
 print(output)
