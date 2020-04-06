e5e9cc7687851e05da663323e6497ab1f57d2ea5
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-06 09:07:04 -0300

    final

---
+++ week3/ex2_week3.py
@@ -0,0 +1,13 @@
+#Create a variable, b, and assign it the value of 15. Then, write code to see if the value b is greater than that of a. If it is, a’s value should be multiplied by 2. If the value of b is less than or equal to a, nothing should happen. Finally, create variable c and assign it the value of the sum of a and b.
+
+a = 20
+b = 15
+
+if b > a:
+    a = a*2
+else:
+    a = a
+    b = b
+
+c = a + b
+print(c)
