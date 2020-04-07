efe471bf7440dcef44ec8157afdbd566356b57fc
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-07 15:25:39 -0300

    final

---
+++ week3/ex25_week3.py
@@ -0,0 +1,9 @@
+#Create the variable z whose value is 30. Write code to see if z is greater than y. If so, add 5 to y’s value, otherwise do nothing. Then, multiply z and y, and assign the resulting value to the variable x.
+
+y = 22
+z = 30
+
+if z > y:
+    y += 5
+    x = z * y
+print(x)
