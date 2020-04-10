543a5fcbf7f99e84019f7e1403fb29a769f2010b
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-10 06:58:15 -0300

    final

---
+++ week4/ex9_week4.py
@@ -0,0 +1,7 @@
+#For each number in lst_nums, multiply that number by 2 and append it to a new list called larger_nums.
+lst_nums = [4, 29, 5.3, 10, 2, 1817, 1967, 9, 31.32]
+larger_nums = []
+
+for i in lst_nums:
+    larger_nums.append(i*2)
+print(larger_nums)
